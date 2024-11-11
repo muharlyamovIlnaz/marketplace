d502ef3cc7e49b5bdd91ef0bcd2b6ebe85dc78fa
muharlyamovilnaz <muharlyashka@gmail.com>
2024-11-11 17:38:42 +0300

    MP-023 Исправил замечания

--- seller-service/src/main/java/com/ilnaz/sellerservice/controller/GoodApi.java
+++ seller-service/src/main/java/com/ilnaz/sellerservice/controller/GoodApi.java
@@ -3,11 +3,6 @@ package com.ilnaz.sellerservice.controller;
 
 import com.ilnaz.sellerservice.dto.GoodDto;
 import com.ilnaz.sellerservice.dto.SellerServiceResponse;
-import io.swagger.v3.oas.annotations.Operation;
-import io.swagger.v3.oas.annotations.media.Content;
-import io.swagger.v3.oas.annotations.media.Schema;
-import io.swagger.v3.oas.annotations.responses.ApiResponse;
-import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -20,48 +15,26 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @RequestMapping("/good")
 @Tag(name = "GoodController")
 public interface GoodApi {
-    @Operation(
-            summary = "Создать товар",
-            description = "Создать товар",
-            tags = "Good"
-    )
-    @ApiResponses({
-            @ApiResponse(
-                    responseCode = "200",
-                    description = "Товар создан",
-                    content = {
-                            @Content(
-                                    mediaType = "application/json",
-                                    schema = @Schema(implementation = GoodDto.class)
-                            )
-                    }
-            ),
-            @ApiResponse(
-                    responseCode = "400",
-                    description = "Неверные данные",
-                    content = {
-                            @Content(
-                                    mediaType = "application/json",
-                                    schema = @Schema(implementation = String.class)
-                            )
-                    }
-            )
-    })
 
     @PostMapping
     SellerServiceResponse createGood(@RequestBody GoodDto goodDto);
 
+
     @DeleteMapping("/{id}")
     SellerServiceResponse deleteGood(@PathVariable(name = "id") long id);
+
 
     @PatchMapping
     SellerServiceResponse updateGood(@RequestBody GoodDto goodDto);
 
+
     @GetMapping("/{id}")
     SellerServiceResponse getGoodById(@PathVariable(name = "id") long id);
 
+
     @GetMapping("/seller/{id}")
     SellerServiceResponse getAllGoods(@PathVariable(name = "id") long sellerId);
+
 
     @GetMapping("/group/{id}")
     SellerServiceResponse getGoodsByGroupId(@PathVariable(name = "id") long goodCategoryId);
